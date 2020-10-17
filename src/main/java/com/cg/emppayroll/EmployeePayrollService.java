e94e3a970f0695825264601b57f038bce1ba3a63
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-17 22:19:51 +0530

    [Siddhartha] refactored: consol io

--- src/main/java/com/cg/emppayroll/EmployeePayrollService.java
+++ src/main/java/com/cg/emppayroll/EmployeePayrollService.java
@@ -2,6 +2,7 @@ package com.cg.emppayroll;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class EmployeePayrollService {
 
@@ -17,6 +18,25 @@ public class EmployeePayrollService {
 
 	public EmployeePayrollService(List<EmployeePayrollData> list) {
 		this.list = list;
+	}
+	public void readEmpPayrollData(Scanner sc) {
+		System.out.println("Enter employee id: ");
+		int id= sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter employee name: ");
+		String name=sc.nextLine();
+		System.out.println("Enter employee salary: ");
+		double sal=sc.nextDouble();
+		list.add(new EmployeePayrollData(id,name,sal));
+	}
+	public void writeEmployeePayrollData() {
+		System.out.println(list);
+	}
+	public static void main(String... args) {
+		Scanner sc=new Scanner(System.in);
+		EmployeePayrollService serviceObj=new EmployeePayrollService();
+		serviceObj.readEmpPayrollData(sc);
+		serviceObj.writeEmployeePayrollData();
 	}
 
 }
