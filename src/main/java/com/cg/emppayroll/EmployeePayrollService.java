976ed6d2125d2d659e091a09748460bf14a6f22f
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-18 01:26:19 +0530

    [Siddhartha] refactored: writing to a file

--- src/main/java/com/cg/emppayroll/EmployeePayrollService.java
+++ src/main/java/com/cg/emppayroll/EmployeePayrollService.java
@@ -19,24 +19,27 @@ public class EmployeePayrollService {
 	public EmployeePayrollService(List<EmployeePayrollData> list) {
 		this.list = list;
 	}
+
 	public void readEmpPayrollData(Scanner sc) {
 		System.out.println("Enter employee id: ");
-		int id= sc.nextInt();
+		int id = sc.nextInt();
 		sc.nextLine();
 		System.out.println("Enter employee name: ");
-		String name=sc.nextLine();
+		String name = sc.nextLine();
 		System.out.println("Enter employee salary: ");
-		double sal=sc.nextDouble();
-		list.add(new EmployeePayrollData(id,name,sal));
-	}
-	public void writeEmployeePayrollData() {
-		System.out.println(list);
-	}
-	public static void main(String... args) {
-		Scanner sc=new Scanner(System.in);
-		EmployeePayrollService serviceObj=new EmployeePayrollService();
-		serviceObj.readEmpPayrollData(sc);
-		serviceObj.writeEmployeePayrollData();
+		double sal = sc.nextDouble();
+		list.add(new EmployeePayrollData(id, name, sal));
 	}
 
+	public void writeEmployeePayrollData(IOService ioService) {
+		if (ioService.equals(IOService.CONSOLE_IO))
+			System.out.println(list);
+		else if (ioService.equals(IOService.FILE_IO))
+			new EmployeePayrollFileIOService().writeData(this.list);
+	}
+	public long countEntries(IOService ioService) {
+		if(ioService.equals(IOService.FILE_IO))
+			return new EmployeePayrollFileIOService().countEntries();
+		return 0;
+	}
 }
---
+++ src/test/java/com/cg/emppayroll/EmployeePayrollServiceTest.java
@@ -0,0 +1,21 @@
+package com.cg.emppayroll;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EmployeePayrollServiceTest {
+	
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchNumberOfEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmployees = { 
+				new EmployeePayrollData(1, "Arijit dey", 1000.0),
+				new EmployeePayrollData(2, "Kalyan Arigela", 1100.0),
+				new EmployeePayrollData(3, "Anand Kumar", 1500.0) };
+		EmployeePayrollService empPayrollService; 
+		empPayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+		empPayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+		Assert.assertEquals(3, empPayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO));
+	}
+}
